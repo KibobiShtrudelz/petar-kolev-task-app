75f9793762845c873a9de86dfcea253ecf19340a
k0l3v <petarko@tryond.com>
2019-10-29 17:08:35 +0200

    76. Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -18,10 +18,63 @@ client.connect(err => {
   console.log("Connected correctly!");
 
   const db = client.db(databaseName);
-  const collection = db.collection("users").insertOne({
-    name: "Petar",
-    age: 30
-  });
+  // db.collection("users").insertOne(
+  //   {
+  //     name: "Petar",
+  //     age: 30
+  //   },
+  //   (err, result) => {
+  //     if (err) {
+  //       return console.log("Unable to insert user!");
+  //     }
+
+  //     console.log(result.ops);
+  //   }
+  // );
+
+  // db.collection("users").insertMany(
+  //   [
+  //     {
+  //       name: "Jen",
+  //       age: 18
+  //     },
+  //     {
+  //       name: "Gunter",
+  //       age: 42
+  //     }
+  //   ],
+  //   (err, result) => {
+  // if (err) {
+  //   return console.log("Unable to insert user!");
+  // }
+
+  // console.log(result.ops);
+  //   }
+  // );
+
+  db.collection("tasks").insertMany(
+    [
+      {
+        description: "Task 1",
+        completed: true
+      },
+      {
+        description: "Task 2",
+        completed: false
+      },
+      {
+        description: "Task 3",
+        completed: true
+      }
+    ],
+    (err, result) => {
+      if (err) {
+        return console.log("Unable to insert tasks!");
+      }
+
+      console.log(result.ops);
+    }
+  );
 
   client.close();
 });
