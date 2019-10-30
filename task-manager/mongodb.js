4d895a1074499d452330a578a98dee7325afa365
k0l3v <petarko@tryond.com>
2019-10-30 10:56:22 +0200

    End of Section 10: MongoDB and Promises (Task App)

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,4 +1,5 @@
-const MongoClient = require("mongodb").MongoClient;
+// const mongodb = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
 
 const uri =
   "mongodb+srv://petarkolevtaskapp:petarkolevtaskapp@petar-kolev-task-app-xqylp.mongodb.net/task-manager?retryWrites=true&w=majority";
@@ -15,66 +16,13 @@ client.connect(err => {
     return console.log("Unable to connect to database!");
   }
 
-  console.log("Connected correctly!");
-
+  console.log("Connected successfully!");
   const db = client.db(databaseName);
-  // db.collection("users").insertOne(
-  //   {
-  //     name: "Petar",
-  //     age: 30
-  //   },
-  //   (err, result) => {
-  //     if (err) {
-  //       return console.log("Unable to insert user!");
-  //     }
 
-  //     console.log(result.ops);
-  //   }
-  // );
-
-  // db.collection("users").insertMany(
-  //   [
-  //     {
-  //       name: "Jen",
-  //       age: 18
-  //     },
-  //     {
-  //       name: "Gunter",
-  //       age: 42
-  //     }
-  //   ],
-  //   (err, result) => {
-  // if (err) {
-  //   return console.log("Unable to insert user!");
-  // }
-
-  // console.log(result.ops);
-  //   }
-  // );
-
-  db.collection("tasks").insertMany(
-    [
-      {
-        description: "Task 1",
-        completed: true
-      },
-      {
-        description: "Task 2",
-        completed: false
-      },
-      {
-        description: "Task 3",
-        completed: true
-      }
-    ],
-    (err, result) => {
-      if (err) {
-        return console.log("Unable to insert tasks!");
-      }
-
-      console.log(result.ops);
-    }
-  );
+  db.collection("users")
+    .deleteOne({ age: 30 })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
 
   client.close();
 });
